d8a83667fe5a710c4de4d74ed990a5a4b2e8d47c
Clouds <Clouds.Flowing@gmail.com>
2017-12-15 06:50:51 +0800

    fix space indent

--- src/c/app.js
+++ src/c/app.js
@@ -1,12 +1,12 @@
 // Function to send a message to the Pebble using AppMessage API
 // We are currently only sending a message using the "status" appKey defined in appinfo.json/Settings
 function sendMessage() {
-	Pebble.sendAppMessage({"status": 1}, messageSuccessHandler, messageFailureHandler);
+  Pebble.sendAppMessage({"status": 1}, messageSuccessHandler, messageFailureHandler);
 }
 
 // Called when the message send attempt succeeds
 function messageSuccessHandler() {
-  console.log("Message send succeeded.");  
+  console.log("Message send succeeded.");
 }
 
 // Called when the message send attempt fails
@@ -20,7 +20,7 @@ Pebble.addEventListener("ready", function(e) {
   console.log("JS is ready!");
   sendMessage();
 });
-												
+
 // Called when incoming message from the Pebble is received
 // We are currently only checking the "message" appKey defined in appinfo.json/Settings
 Pebble.addEventListener("appmessage", function(e) {
